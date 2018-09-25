e9cc60ff065d6218231f9c6392e365a424f91a24
Faas <tfaas@in-info-355-inm.soic.iupui.edu>
2018-09-25 14:30:27 -0400

    Adding componentWillUpdate

--- _examples/Week5/component_communication/src/WinMeter.js
+++ _examples/Week5/component_communication/src/WinMeter.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 class WinMeter extends Component {
 
+    state = {
+        message: "Try your best!"
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        if(nextProps.score > this.props.score) {
+            setTimeout(() => { this.cheerOn() }, 2000);
+        }
+    }
+
+    cheerOn() {
+        this.setState({message: "Go go go!"});
+    }
+
     render() {
         return(
             <div>
-                { this.props.score }
+                <div>{ this.state.message }</div>
+                <div>{ this.props.score }</div>
             </div>
         )
     }
